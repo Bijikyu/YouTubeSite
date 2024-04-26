b5d69b70d7de9f4912f1d7d05dc315b160919129
Momo <cocomojomomo@protonmail.com>
2024-04-26 00:14:17 +0000

    Add code comments

--- client/src/components/views/VideoDetailPage/Sections/SingleComment.js
+++ client/src/components/views/VideoDetailPage/Sections/SingleComment.js
@@ -1,75 +1,77 @@
-import React,{useState} from 'react'
-import {Comment, Avatar, Button, Input} from 'antd';
-import {useSelector} from 'react-redux';
-import Axios from 'axios';
-import LikeDislikes from './LikeDislikes';
+// SingleComment component: This file defines a React component that represents a single comment with reply and like/dislike functionality.
 
-function SingleComment(props) {
+import React,{useState} from 'react' // Importing React and the useState hook from react package
+import {Comment, Avatar, Button, Input} from 'antd'; // Importing Comment, Avatar, Button, and Input components from antd library
+import {useSelector} from 'react-redux'; // Importing useSelector hook from react-redux to access the Redux store state
+import Axios from 'axios'; // Importing Axios for making HTTP requests
+import LikeDislikes from './LikeDislikes'; // Importing LikeDislikes component for like and dislike functionality
 
-    const user = useSelector(state => state.user)
-    const [CommentValue, setCommentValue] = useState('')
-    const [OpenReply, setOpenReply] = useState(false)
+function SingleComment(props) { // SingleComment component definition
+
+    const user = useSelector(state => state.user) // Accessing user state from Redux store
+    const [CommentValue, setCommentValue] = useState('') // State to keep track of the comment input value
+    const [OpenReply, setOpenReply] = useState(false) // State to control the visibility of the reply input
     
-    const onClickReplyOpen = () => {
-        setOpenReply(!OpenReply)
+    const onClickReplyOpen = () => { // Handler for toggling the reply input visibility
+        setOpenReply(!OpenReply) // Toggling the OpenReply state
     }
 
-    const onHandleChange = (event) => {
-        setCommentValue(event.currentTarget.value)
+    const onHandleChange = (event) => { // Handler for updating the comment input value
+        setCommentValue(event.currentTarget.value) // Updating the CommentValue state with the new input value
     }
 
-    const onSubmit = (event) => {
-        event.preventDefault();
+    const onSubmit = (event) => { // Handler for submitting the comment
+        event.preventDefault(); // Preventing the default form submit action
 
-        const variables = {
-            content: CommentValue,
-            writer: user.userData._id,
-            postId: props.postId,
-            responseTo: props.comment._id
+        const variables = { // Object containing the comment data to be sent to the server
+            content: CommentValue, // Comment content
+            writer: user.userData._id, // ID of the user writing the comment
+            postId: props.postId, // ID of the post the comment belongs to
+            responseTo: props.comment._id // ID of the comment being replied to (if any)
         }
 
-        Axios.post('/api/comment/saveComment',variables)
-        .then(response => {
-            if(response.data.success){
-                props.refreshFunction(response.data.result)
-                setCommentValue("")
-                setOpenReply(!OpenReply)
+        Axios.post('/api/comment/saveComment',variables) // Making a POST request to save the comment
+        .then(response => { // Handling the response after the request is made
+            if(response.data.success){ // Checking if the comment was saved successfully
+                props.refreshFunction(response.data.result) // Calling the refresh function passed as a prop with the new comment data
+                setCommentValue("") // Resetting the comment input value
+                setOpenReply(!OpenReply) // Toggling the reply input visibility
             }
-            else{
-                alert('커멘트를 저장하지 못했습니다.')
+            else{ // If the comment was not saved successfully
+                alert('커멘트를 저장하지 못했습니다.') // Displaying an error message
             }
         })
     }
 
-    const actions = [
-        <LikeDislikes userId={localStorage.getItem('userId')} commentId={props.comment._id}/>
-        ,<span onClick={onClickReplyOpen} key="comment-basic-reply-to">Reply to</span>
+    const actions = [ // Array of action elements to be passed to the Comment component
+        <LikeDislikes userId={localStorage.getItem('userId')} commentId={props.comment._id}/> // LikeDislikes component for handling likes and dislikes
+        ,<span onClick={onClickReplyOpen} key="comment-basic-reply-to">Reply to</span> // Span element to toggle the reply input visibility
     ]
 
-    return (
-        <div>
-            <Comment
-                actions={actions}
-                author={props.comment.writer.name}
-                avatar={<Avatar src={props.comment.writer.image} al/>}
-                content={<p>{props.comment.content}</p>}
+    return ( // JSX to render the SingleComment component
+        <div> // Container div for the comment and reply form
+            <Comment // Ant Design Comment component to display the comment
+                actions={actions} // Actions to display under the comment
+                author={props.comment.writer.name} // Author name of the comment
+                avatar={<Avatar src={props.comment.writer.image} al/>} // Avatar of the comment author
+                content={<p>{props.comment.content}</p>} // Content of the comment
             />
 
-            {OpenReply &&
-                <form style={{display:'flex'}} onSubmit={onSubmit}>
-                <textarea
-                    style={{width:'100%', borderRadius: '5px'}}
-                    onChange={onHandleChange}
-                    value={CommentValue}
-                    placeholder="코멘트를 작성해주세요"
+            {OpenReply && // Conditional rendering to display the reply form only when OpenReply is true
+                <form style={{display:'flex'}} onSubmit={onSubmit}> // Form element for submitting a reply
+                <textarea // Textarea for the reply input
+                    style={{width:'100%', borderRadius: '5px'}} // Styling for the textarea
+                    onChange={onHandleChange} // Handler to update the reply input value
+                    value={CommentValue} // Value of the reply input
+                    placeholder="코멘트를 작성해주세요" // Placeholder text for the textarea
                 />
-                <br/>
-                <button style={{width:'20%', height: '52px'}} onClick={onSubmit}>Submit</button>
+                <br/> // Line break for layout purposes
+                <button style={{width:'20%', height: '52px'}} onClick={onSubmit}>Submit</button> // Button to submit the reply
                 </form>}
 
              
-        </div>
+        </div> // Closing container div
     )
 }
 
-export default SingleComment
+export default SingleComment // Exporting the SingleComment component for use in other files
--- client/src/components/views/VideoDetailPage/Sections/Subcribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subcribe.js
@@ -1,80 +1,82 @@
-import React,{useEffect, useState} from 'react';
-import Axios from 'axios';
+// This is a React component that allows users to subscribe or unsubscribe to a content creator. It displays the number of subscribers and changes the subscription state based on user interaction.
 
-function Subcribe(props) {
+import React,{useEffect, useState} from 'react'; // Importing React hooks from the react library.
+import Axios from 'axios'; // Importing Axios for making HTTP requests.
 
-    const [SubscribeNumber, setSubscribeNumber] = useState(0)
-    const [Subscribed, setSubscribed] = useState(false)
-   
-    useEffect(()=>{
+function Subcribe(props) { // This is a React functional component for handling subscriptions.
+
+    const [SubscribeNumber, setSubscribeNumber] = useState(0) // State hook for tracking the number of subscribers.
+    const [Subscribed, setSubscribed] = useState(false) // State hook for tracking the subscription status.
+
+    useEffect(()=>{ // This useEffect hook runs once after the initial render to fetch subscription data.
         
-        let variable = {userTo: props.userTo}
+        let variable = {userTo: props.userTo} // Object containing the user to subscribe to.
 
-        Axios.post('/api/subscribe/subscribeNumber',variable)
-            .then(response => {
-                if(response.data.success){
-                    setSubscribeNumber(response.data.subscribeNumber)
+        Axios.post('/api/subscribe/subscribeNumber',variable) // HTTP POST request to get the number of subscribers.
+            .then(response => { // Handling the promise after the request is made.
+                if(response.data.success){ // Checking if the request was successful.
+                    setSubscribeNumber(response.data.subscribeNumber) // Updating the state with the number of subscribers.
                 }else{
-                    alert('구독자 수 정보를 받아오지 못했습니다.')
+                    alert('구독자 수 정보를 받아오지 못했습니다.') // Alerting the user if the request failed.
                 }
             })
 
-            let subscribedVariable = {userTo: props.userTo, userFrom: localStorage.getItem('userId')}
+            let subscribedVariable = {userTo: props.userTo, userFrom: localStorage.getItem('userId')} // Object containing the subscriber and the user to subscribe to.
 
-        Axios.post('/api/subscribe/subscribed',subscribedVariable)
-        .then(response => {
-            if(response.data.success){
-                setSubscribed(response.data.subscribed)
+        Axios.post('/api/subscribe/subscribed',subscribedVariable) // HTTP POST request to check if the user is subscribed.
+        .then(response => { // Handling the promise after the request is made.
+            if(response.data.success){ // Checking if the request was successful.
+                setSubscribed(response.data.subscribed) // Updating the state with the subscription status.
             }else{
-                alert('정보를 받아오지 못했습니다.')
+                alert('정보를 받아오지 못했습니다.') // Alerting the user if the request failed.
             }
         })
 
-    },[])
+    },[]) // The empty array ensures this effect runs only once.
 
-    const onSubscribe = () => {
+    const onSubscribe = () => { // This function handles the subscribe and unsubscribe actions.
 
-        let subscribedVariable = {
+        let subscribedVariable = { // Object containing the subscriber and the user to subscribe to.
 
             userTo: props.userTo,
             userFrom: props.userFrom
         }
 
-        if(Subscribed){
-            Axios.post('/api/subscribe/unSubscribe',subscribedVariable)
-                .then(response => {
-                    if(response.data.success){
-                        setSubscribeNumber(SubscribeNumber-1)
-                        setSubscribed(!Subscribed)
+        if(Subscribed){ // Checking if the user is already subscribed.
+            Axios.post('/api/subscribe/unSubscribe',subscribedVariable) // HTTP POST request to unsubscribe the user.
+                .then(response => { // Handling the promise after the request is made.
+                    if(response.data.success){ // Checking if the request was successful.
+                        setSubscribeNumber(SubscribeNumber-1) // Decreasing the number of subscribers.
+                        setSubscribed(!Subscribed) // Updating the subscription status.
                     }else{
-                        alert('구독 취소하는데 실패했습니다.')
+                        alert('구독 취소하는데 실패했습니다.') // Alerting the user if the request failed.
                     }
                 })
         }
         else{
-            Axios.post('/api/subscribe/subscribe',subscribedVariable)
-                .then(response => {
-                    if(response.data.success){
-                        setSubscribeNumber(SubscribeNumber+1)
-                        setSubscribed(!Subscribed)
+            Axios.post('/api/subscribe/subscribe',subscribedVariable) // HTTP POST request to subscribe the user.
+                .then(response => { // Handling the promise after the request is made.
+                    if(response.data.success){ // Checking if the request was successful.
+                        setSubscribeNumber(SubscribeNumber+1) // Increasing the number of subscribers.
+                        setSubscribed(!Subscribed) // Updating the subscription status.
                     }else{
-                        alert('구독하는데 실패했습니다.')
+                        alert('구독하는데 실패했습니다.') // Alerting the user if the request failed.
                     }
                 })
         }
     }
    
-    return (
+    return ( // This return statement renders the subscribe button with dynamic styles and text.
         <div>
             <button
-            onClick={onSubscribe}
-            style={{backgroundColor:`${Subscribed ? '#AAAAAA' : '#CC0000' }`, borderRadius:'4px',
+            onClick={onSubscribe} // Event handler for the click event on the subscribe button.
+            style={{backgroundColor:`${Subscribed ? '#AAAAAA' : '#CC0000' }`, borderRadius:'4px', // Conditional styling based on subscription status.
             color: 'white', padding: '10px 16px', fontWeight: '500', fontSize:'1rem', textTransform:'uppercase'   
             }} >
-            {SubscribeNumber}  {Subscribed ? 'Subscribed':'Subscribe'}
+            {SubscribeNumber}  {Subscribed ? 'Subscribed':'Subscribe'} // Displaying the number of subscribers and subscription status.
             </button>
         </div>
     )
 }
 
-export default Subcribe
+export default Subcribe // Exporting the Subscribe component for use in other parts of the application.
